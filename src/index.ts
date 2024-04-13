6a30cf71670c8915f218c02bc72943b817182b8a
Kaialan <satyajit1501@gmail.com>
2024-04-13 18:40:22 +0530

    basic implementation of gemini

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+//Importing project dependancies that we installed earlier
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors' 
+const { GoogleGenerativeAI } = require("@google/generative-ai")
+
+
+
+dotenv.config()
+
+
+const app = express(); 
+
+ 
+app.use(cors()); 
+app.use(express.json())
+
+
+// module.exports = app
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, async () => {
+   console.log(`[server]: Server listning on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+   res.send("Hello from server\n")
+})
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+app.post('/chat', async (req, res) => {
+   const prompt = req.body.prompt;
+
+   console.log(prompt)
+   
+   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+   
+   const result = await model.generateContent(prompt);
+   const response = await result.response;
+   const text = response.text();
+   console.log(response);
+   
+   res.send(text).status(200)
+})
